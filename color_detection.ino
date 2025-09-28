62b887afb8d68e6ef53803a6743b47b1e42583d5
ZeuPark <parkzeu68@gmail.com>
2025-09-28 11:17:24 +1000

    Update color_detection.ino

--- color_detection.ino
+++ color_detection.ino
@@ -1,24 +1,5 @@
-//
-// begin license header
-//
-// This file is part of Pixy CMUcam5 or "Pixy" for short
-//
-// All Pixy source code is provided under the terms of the
-// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
-// Those wishing to use Pixy source code, software and/or
-// technologies under different licensing terms should contact us at
-// cmucam@cs.cmu.edu. Such licensing terms are available for
-// all portions of the Pixy codebase presented here.
-//
-// end license header
-//
-// This sketch is a good place to start if you're just getting started with 
-// Pixy and Arduino.  This program simply prints the detected object blocks 
-// (including color codes) through the serial console.  It uses the Arduino's 
-// ICSP SPI port.  For more information go here:
-//
 // https://docs.pixycam.com/wiki/doku.php?id=wiki:v2:hooking_up_pixy_to_a_microcontroller_-28like_an_arduino-29
-//
+
   
 #include <Pixy2.h>
 
@@ -49,7 +30,31 @@ void loop()
       Serial.print("  block ");
       Serial.print(i);
       Serial.print(": ");
-      pixy.ccc.blocks[i].print();
+      // Print signature (color)
+      Serial.print("color: ");
+      switch(pixy.ccc.blocks[i].m_signature)
+      {
+        case 1:
+          Serial.print("Red");
+          break;
+        case 2:
+          Serial.print("Green");
+          break;
+        case 3:
+          Serial.print("Blue");
+          break;
+        // Add more cases for other signatures you've trained
+        default:
+          Serial.print("Signature ");
+          Serial.print(pixy.ccc.blocks[i].m_signature);
+          break;
+      }
+
+      // Print range estimate (based on object size)
+      // A larger area means the object is closer.
+      int32_t area = (int32_t)pixy.ccc.blocks[i].m_width * (int32_t)pixy.ccc.blocks[i].m_height;
+      Serial.print(", range estimate (area): ");
+      Serial.println(area);
     }
   }  
 }
